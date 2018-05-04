7888c08e9c818ab223477bc9bcd9b31c65ef86c2
Simon <38388627+simonau866@users.noreply.github.com>
2018-05-04 19:42:26 +1000

    Update myImage.cpp

--- VS2013 Version/HandDetection/myImage.cpp
+++ VS2013 Version/HandDetection/myImage.cpp
@@ -1,19 +1,13 @@
-#include "myImage.hpp"
-#include <opencv2/imgproc/imgproc.hpp>
-#include<opencv2/opencv.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <stdio.h>
-#include <stdlib.h>
-#include <iostream>
-#include <string>
+#include "myImage.h"
 
 using namespace cv;
 
-MyImage::MyImage(){
+MyImage::MyImage()
+{
 }
 
-MyImage::MyImage(int webCamera){
-	cameraSrc=webCamera;
-	cap=VideoCapture(webCamera);
+MyImage::MyImage(int webCamera)
+{
+	cameraSrc = webCamera;
+	cap = VideoCapture(webCamera);
 }
-
